5dd6600ac5e79b30b6476d400f2b1ee2ee28d464
Nwajeiiii <nwajeiolufemi@gmail.com>
2023-01-31 14:06:51 +0100

    Initial Commit

--- src/com/olufemi/questionthree/Main.java
+++ src/com/olufemi/questionthree/Main.java
@@ -5,23 +5,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Vehicle.Owner owner = new Vehicle("Toyota", "1000cc", "GR86").new Owner("Nwajei Olufemi", 234, "09052289046");
+//        over-riding the method of a class using anonymous inner class
+        Vehicle vehicle = new Vehicle("Toyota", "1000cc", "GR86"){
 
-//        Vehicle vehicle = new Vehicle("Toyota", "1000cc", "GR86");
-//        Vehicle.Owner owner = vehicle.new Owner("Nwajei Olufemi", 234, "09052289046");
+            @Override
+            public void setEngineCC(String engineCC) {
+                super.setEngineCC(engineCC);
+            }
+        };
 
-//        overridding the method of a class using anonymous inner class
-//        Vehicle vehicle = new Vehicle("Toyota", "1000cc", "GR86");
-//        {
-//
-//            @Override
-//            public void setEngineCC(String engineCC) {
-////                super.setEngineCC(engineCC);
-//                System.out.println("jhbcvdvsticstivstiviastvsatvtas7v");
-//            }
-//        };
-//
-//        vehicle.setEngineCC("ncsnc");
 
 //    pass an anonymous inner class as method argument
 
